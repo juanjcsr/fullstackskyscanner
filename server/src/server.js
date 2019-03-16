866daecbfdca836924366eb3b3a05ede924ff816
Juan Carlos Sanchez <juanjcsr@gmail.com>
2019-03-16 13:58:24 -0600

    initial commit

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@
 // Disabling 'no-console' as it's reasonable for this file to do some logging.
 
 const express = require('express');
+const _ = require('lodash');
 
 const app = express();
 const livePricing = require('./live-pricing');
@@ -26,16 +27,83 @@ app.get('/', (req, res) => {
 */
 app.get('/api/search', async (req, res) => {
   try {
+
+    const p = {
+      OriginPlace: "EDI",
+      DestinationPlace: "LHR",
+      OutboundDate:"2019-03-18",
+      InboundDate:"2019-03-19",
+      Adults:1,
+      Children:0,
+      Infants:0,
+      pageIndex:0,
+      pageSize: 10
+    };
     const results = await livePricing.search({
     /*
      TODO: client to provide params.
      Some params are already provided for you - see live-pricing.js.
      Check API docs to see the other params you need to provide.
      */
+      ...p
     });
     // TODO - a better format for displaying results to the client
     console.log('TODO: transform results for consumption by client');
-    res.json(results);
+    // let out = _.groupBy(res.Itineraries, 'OutboundLegId')
+    let carriers = _.groupBy(results.Carriers, 'Id');
+    let agents = _.groupBy(results.Agents, 'Id');
+    let places = _.groupBy(results.Places, 'Id');
+    let segments = _.groupBy(results.Segments, 'Id')
+    let legs = _.groupBy(results.Legs, 'Id');
+    
+    let itineraries = {
+      itineraries: [],
+      agents,
+      places,
+      segments,
+      legs,
+      carriers
+    };
+    itineraries.Itineraries = []
+     _.map(results.Itineraries, (s) => {
+      outboundLeg = legs[s.OutboundLegId][0]
+      inboundLeg = legs[s.InboundLegId][0]
+      outboundSegments = [];
+      inboundSegments = [];
+      console.log(outboundLeg.Id)
+      outboundLeg.SegmentIds.map( id => {
+        outboundSegments.push(segments[id][0]);
+      })
+
+
+      inboundLeg.SegmentIds.map( id => {
+        inboundSegments.push(segments[id][0])
+      })
+
+      outboundSegments.map( seg => {
+        seg.OriginStationPlace = places[seg.OriginStation];
+        seg.DestinationStation = places[seg.DestinationStation];
+        seg.CarrierName = carriers[seg.Carrier];
+        seg.OperatingCarrier = carriers[seg.OperatingCarrier];
+      })
+
+      inboundSegments.map( seg => {
+        seg.OriginStationPlace = places[seg.OriginStation];
+        seg.DestinationStation = places[seg.DestinationStation];
+        seg.CarrierName = carriers[seg.Carrier];
+        seg.OperatingCarrier = carriers[seg.OperatingCarrier];
+      })
+
+      outboundLeg.SegmentsDetail = outboundSegments;
+      inboundLeg.SegmentsDetail = inboundSegments;
+
+      s.OutboundLeg = outboundLeg
+      s.InboundLeg = inboundLeg
+
+      itineraries.itineraries.push(s)
+    })
+
+    res.json(itineraries);
   } catch (err) {
     res.status(500).send(err);
     console.error(err);
